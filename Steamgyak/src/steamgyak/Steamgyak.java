b16a0ca67a48a5fc7228dc329ecce4560eb90e8e
Tetanki-HUN <85568371+Tetanki-HUN@users.noreply.github.com>
2022-10-25 10:28:45 +0200

    Dolgozatmegoldás

---
+++ Steamgyak/src/steamgyak/Steamgyak.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package steamgyak;
+
+/**
+ *
+ * @author user3
+ */
+public class Steamgyak {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String osztaly = "Vad alma ";
+        
+        String nev = osztaly.toUpperCase();
+        System.out.println(nev);
+        
+        
+        System.out.println(osztaly.replace("alma", "körte"));
+        
+        //email cim hozza minden kisbetű, szóköz benne pont, osztály id
+        
+        
+    }
+    
+}
