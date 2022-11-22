cdc93f15afb87cbb536ab88e490d715b506fc6ec
Tetanki-HUN <85568371+Tetanki-HUN@users.noreply.github.com>
2022-11-22 13:17:50 +0100

    Dolgozatmegoldás

---
+++ gyakorlasmetodusok11.22/src/gyakorlasmetodusok11/pkg22/Gyakorlasmetodusok1122.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gyakorlasmetodusok11.pkg22;
+
+/**
+ *
+ * @author user3
+ */
+import java.util.Scanner;
+public class Gyakorlasmetodusok1122 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        boolean novekvo = false;
+        System.out.println("Adj meg egy számot: ");
+        int szam1 = myObj.nextInt();
+        System.out.println("Adj meg még egy számot: ");
+        int szam2 = myObj.nextInt();
+        System.out.println("Adj meg egy harmadik számot: ");
+        int szam3 = myObj.nextInt();
+        
+        
+        int szam = 72;
+        Character.getNumericValue('1');
+        var valueOf = String.valueOf(szam);
+        System.out.println(szam);
+        
+    }
+    
+     
+    
+}
