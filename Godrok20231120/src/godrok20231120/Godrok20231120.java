6fdf2829b1e31277c072e06815b92248964fb5a4
Gulyás Zsolt Máté <85568371+Tetanki-HUN@users.noreply.github.com>
2023-11-20 09:31:11 +0100

    Godrok keresése

--- Godrok20231120/src/godrok20231120/Godrok20231120.java
+++ Godrok20231120/src/godrok20231120/Godrok20231120.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package godrok20231120;
-
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
 /**
  *
  * @author MSI
@@ -14,7 +16,28 @@ public class Godrok20231120 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        RandomAccessFile raf;
+        ArrayList<Tancok> parok = new ArrayList<>();  
+        
+        try {
+            raf = new RandomAccessFile("melyseg.txt","rw");
+                            
+            String fiuTanc = " ";
+            
+            while( fiuTanc != null ) {
+                String tanc = raf.readLine();
+                String lanyTancs = raf.readLine();
+                fiuTanc = raf.readLine();
+                String lanyTanc = null;
+                Tancok adatok = new Tancok(tanc, fiuTanc, lanyTanc);
+                parok.add(adatok);
+                
+            }
+            
+            raf.close();
+        } catch( IOException e ) {
+            System.err.println("HIBA");
+        }
     }
     
 }
