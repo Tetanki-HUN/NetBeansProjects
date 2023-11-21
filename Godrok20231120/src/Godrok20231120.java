b57ca6d20d6dc4fee708fb3a39fe4d5fdc09e965
Gulyás Zsolt Máté <85568371+Tetanki-HUN@users.noreply.github.com>
2023-11-21 08:48:52 +0100

    Godrok folytatas

--- Godrok20231120/src/Godrok20231120.java
+++ Godrok20231120/src/Godrok20231120.java
@@ -1,19 +1,33 @@
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Godrok20231120 {
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("melyseg.txt"))) {
             String line;
+            Integer db = 0;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                db++;
+
             }
+            System.out.println("1. feladat");
+            System.out.println(db);
+            System.out.println("--------------------------------------------");
         } catch (IOException e) {
             System.err.println("Hiba: " + e.getMessage());
-        }
+        } 
+        
+        System.out.println("2. feladat");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adjon meg egy távolságértéket: ");
+        int tavolsag = sc.nextInt();
+        int tavolsagdb = 0;
+        
+        if (tavolsag > line)
     }
-    
-    
+
 }
