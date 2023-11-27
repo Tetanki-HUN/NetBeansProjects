ff136a0e737f72d21416c0d79ccdaf2bd4d5c454
Gulyás Zsolt Máté <85568371+Tetanki-HUN@users.noreply.github.com>
2023-11-27 17:19:34 +0100

    Működő beolvasás Arraylistet használva

--- Godrok20231120/src/Godrok20231120.java
+++ Godrok20231120/src/Godrok20231120.java
@@ -1,47 +1,36 @@
-
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Godrok20231120 {
 
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("melyseg.txt"))) {
-            String line;
-            Integer db = 0;
-            while ((line = br.readLine()) != null) {
-                db++;
+        ArrayList<Integer> numbers = new ArrayList<>();
 
+        try (Scanner fileScanner = new Scanner(new FileReader("melyseg.txt"))) {
+            while (fileScanner.hasNextInt()) {
+                int number = fileScanner.nextInt();
+                numbers.add(number);
             }
             System.out.println("1. feladat");
-            System.out.println(db);
+            System.out.println(numbers.size());
             System.out.println("--------------------------------------------");
         } catch (IOException e) {
             System.err.println("Hiba: " + e.getMessage());
-        } 
-        
+        }
+
         System.out.println("2. feladat");
         Scanner sc = new Scanner(System.in);
         System.out.println("Adjon meg egy távolságértéket: ");
         int tavolsag = sc.nextInt();
         int tavolsagdb = 0;
-        
-        try (BufferedReader br = new BufferedReader(new FileReader("melyseg.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                int number = Integer.parseInt(line.trim());
-                if (number > tavolsag) {
-                    tavolsagdb++;
-                }
-            }
-            System.out.println("A megadott távolságot meghaladó számok száma: " + tavolsagdb);
-        } catch (IOException | NumberFormatException e) {
-            System.err.println("Hiba: " + e.getMessage());
-        }
-        
-        
-        
-    }
 
+        for (int number : numbers) {
+            if (number > tavolsag) {
+                tavolsagdb++;
+            }
+        }
+        System.out.println("A megadott távolságot meghaladó számok száma: " + tavolsagdb);
+    }
 }
